671d825f7d3b731f98eb2b22897999a1aa2bd778
MayCardona <mcardonas@sura.com.co>
2025-10-31 08:36:54 -0500

    cambios en chockout

--- Reservas_Salas.Server/obj/Debug/net8.0/Reservas_Salas.Server.AssemblyInfo.cs
+++ Reservas_Salas.Server/obj/Debug/net8.0/Reservas_Salas.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Reservas_Salas.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38ac1daf1fc1e368d863bd59c5cd1c7238b91f5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Reservas_Salas.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Reservas_Salas.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
